9a930509c2804944b01c1644d6f341d5c87dcb54
Yuki-jukjis <taka0618.m@gmail.com>
2020-03-11 00:15:14 +0900

    Update breadcrumbs.js

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   // URLを取得
   let path = location.pathname;
+  //最初の/を除去
+  path = path.substring(1);
   //URLを整形（拡張子やindex.htmlを除去）
   if(path.lastIndexOf("/") < path.lastIndexOf(".")) {
     path = path.substring(0, path.lastIndexOf("."));
@@ -8,6 +10,9 @@ document.addEventListener("DOMContentLoaded", function() {
       path = path.substring(0, path.lastIndexOf("/"));
     }
   }
+  //最後の/を除去
+  if(path.endsWith("/"))
+    path = path.substring(0, path.length - 1);
   let splited = path.split("/");
   let target = document.getElementById("breadcrumbs");
   splited.forEach((x,i)=>{
