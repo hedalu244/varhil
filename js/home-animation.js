4a66b353930f5a75d7a0481918a715c911d1a969
Yuki-jukjis <taka0618.m@gmail.com>
2020-03-14 19:47:26 +0900

    スクロール

--- js/home-animation.js
+++ js/home-animation.js
@@ -7,10 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   //サイト外から来たとき
   else {
-    $("#draw-svg").css("opacity", 1);
+    forbid_scroll();
+    scrollTo(0, 0);
+    $("#key-visual").css("margin-top", (window.innerHeignt - $(".key-visual").outerHeight()) / 2 + "px");
+    $("#draw-svg").css("opacity", 1)
+
     $('#draw-svg').drawsvg().drawsvg('animate');
     setTimeout(()=>$('#logo').animate({opacity:1}, { duration: 400, easing: 'swing' }), 2300);
+
+    setTimeout($("#key-visual").animate({"margin-top":0}, { duration: 1000, easing: 'swing' })), 3000);
     setTimeout(()=>$('#content').animate({opacity:1}, { duration: 400, easing: 'swing' }), 3000);
+    setTimeout(permit_scroll, 3000);
   }
   // スクロール禁止
   function forbid_scroll() {
