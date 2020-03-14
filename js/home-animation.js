5143e31da1abaac78d8d15d725f15c9ad488067c
Yuki-jukjis <taka0618.m@gmail.com>
2020-03-14 19:19:53 +0900

    Update home-animation.js

--- js/home-animation.js
+++ js/home-animation.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   //直接来たとき||サイト内リンクで来たとき
   if (document.referrer === "" || new URL(document.referrer).hostname === location.hostname){
-    $('#logo').animate({opacity:1}, "slow");
-    $('#draw-svg').animate({opacity:1}, "slow");
+    $('#logo').animate({opacity:1}, { duration: 1000, easing: 'swing' });
+    $('#draw-svg').animate({opacity:1}, { duration: 1000, easing: 'swing' });
   }
   //サイト外から来たとき
   else {
     $("#draw-svg").css("opacity", 1);
     $('#draw-svg').drawsvg().drawsvg('animate');
-    setTimeout(()=>$('#logo').animate({opacity:1}), 2300);
+    setTimeout(()=>$('#logo').animate({opacity:1}, { duration: 400, easing: 'swing' }), 2300);
   }
   // スクロール禁止
   function forbid_scroll() {
